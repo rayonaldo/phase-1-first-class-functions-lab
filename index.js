d8aa4f7ab8a923c48094cbc34773f202ffeacb41
ray omego <ray.omego@student.moringaschool.com>
2022-08-23 11:53:00 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arrayDrivers) => {
+    return arrayDrivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = (arrayDrivers) => {
+    return arrayDrivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiple) => {
+    let callback = (num) => num*multiple;  
+    return callback;
+}
+
+const fareDoubler = (num) => {
+    let callback = createFareMultiplier(2); 
+    return callback(num); 
+}
+
+const fareTripler = (num) => {
+    let callback = createFareMultiplier(3);
+    return callback(num);
+}
+
+const selectDifferentDrivers = (arrayOfDrivers, fn) => {
+    if (fn === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    } else if (fn === returnLastTwoDrivers){
+        return returnLastTwoDrivers(arrayOfDrivers);
+    } else {
+        return "invalid";
+    }
+}
